6c410424ced77d93dea0267fff2533b80a3d1182
massssanari <jugemmu.tf@gmail.com>
2021-07-24 21:56:48 +0900

    パイプライン修正

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,6 @@ module.exports = {
     // 出力するファイル名
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'public/js')
+    path: path.join(__dirname, 'public/js/')
   }
 };
