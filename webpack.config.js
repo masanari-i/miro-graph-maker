31deaf8f460376d613b90aa8703b0acf5432c566
massssanari <jugemmu.tf@gmail.com>
2021-07-24 20:09:25 +0900

    staging修正

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
   mode: 'production',
   // エントリーポイントの設定
-  entry: './js/index.js',
+  entry: './src/js/index.js',
   // 出力の設定
   output: {
     // 出力するファイル名
